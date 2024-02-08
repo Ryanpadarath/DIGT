938044611ac1f00b9aeed5e4be0d874a33c55385
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2024-02-08 15:30:00 +1300

    Update coding_copy_20240208150653.ino
    //shared with mr hansen successfully :)

--- coding_copy_20240208150653/coding_copy_20240208150653.ino
+++ coding_copy_20240208150653/coding_copy_20240208150653.ino
@@ -1,17 +1,20 @@
-const  ledPin=13;
+//shared with mr hansen successfully :)
+
+const byte ledPin=13;
 const byte PAW1=1;
 const byte PAW2=2;
 const byte PAW3=3;
 const byte PAW4=4;
+
 void setup() {
-  pinmode(13, INPUT):// put your setup code here, to run once:
 pinmode(LEDPIN, OUTPUT);
-Pinmode(PAWPIN, INPUT);
 pinmode(PAWPIN1, INPUT);
 Pinmode(PAWPIN2, INPUT);
 pinmode(PAWPIN3, INPUT);
 pinmode(PAWPIN4, INPUT);
+}
+
 void loop() {
-  /void loop() {/ put your main code here, to run repeatedly:
 if(digitalRead(PAWPIN1) == HIGH){
+}
 }
